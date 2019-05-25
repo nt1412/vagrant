8ca30352b2eda475e1e86be35d80a8f8d3afd6b6
John Doe <john@doe.com>
2019-05-25 13:18:01 +0000

    Impersonation is evil.

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo apt-get update && apt-get install -y apt-transport-https curl
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
+
